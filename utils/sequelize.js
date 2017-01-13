e2180ec2cdb272c21b1ef27eeeda4c95e771e296
Matt Jacobs <hello@mattjdev.com>
2017-01-13 12:17:39 -0600

    remove hard coded mysql config for production env

--- utils/sequelize.js
+++ utils/sequelize.js
@@ -1,7 +1,7 @@
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
 var db_config = require('../config/config.json')[env];
-var sequelize = new Sequelize(
+var sequelize = process.env.DB_CONFIG_URL ? new Sequelize(process.env.DB_CONFIG_URL) : new Sequelize(
   db_config.database,
   db_config.username,
   db_config.password,
