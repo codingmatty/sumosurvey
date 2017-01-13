af672927736855320e7c5c1375c91c09dfc50077
Matt Jacobs <hello@mattjdev.com>
2017-01-13 09:30:06 -0600

    Fix db config file

--- utils/sequelize.js
+++ utils/sequelize.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 var env = process.env.NODE_ENV || 'development';
-var db_config = require('../config/config.json').db[env];
+var db_config = require('../config/config.json')[env];
 var sequelize = new Sequelize(
   db_config.database,
   db_config.username,
